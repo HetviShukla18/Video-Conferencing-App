70bcee345970349306c2ec7e5eeaa92afccf2e26
HetviShukla18 <hetvishukla2@gmail.com>
2025-09-19 20:46:57 +0530

    solve issues

--- app/(root)/(home)/recording/page.tsx
+++ app/(root)/(home)/recording/page.tsx
@@ -1,18 +1,9 @@
-import CallList from '@/components/CallList'
 import React from 'react'
 import Image from 'next/image'
 import { CallList } from '@/components/CallList'
 
 const Recording = () => {
   return (
-<<<<<<< HEAD
-    <section className='flex size-full flex-col gap-10 text-white'>
-    <h1 className='text-3xl font-bold'>
-      Recording
-    </h1>
-    <CallList type='recordings ' />
-   </section>
-=======
     <section className="flex size-full flex-col gap-8 text-white">
       <header className="flex items-center gap-4">
         <Image src="/icons/recordings.svg" alt="Recordings" width={36} height={36} />
@@ -24,7 +15,6 @@ const Recording = () => {
 
       <CallList type="recordings" />
     </section>
->>>>>>> 0ca0798 (.)
   )
 }
 
